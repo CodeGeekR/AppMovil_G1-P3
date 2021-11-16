c5a61e261ed1d9ab516a0b07ad76c9d802601b08
sammydn7 <sammydn7@gmail.com>
2021-11-16 13:33:48 -0500

    Creacion de proyecto y Splash Screen

---
+++ app/src/main/java/com/example/kamaleon/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.kamaleon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+
+}
