804e868f5dbd56f3af19b6588822fc67d08379f1
sammydn7 <sammydn7@gmail.com>
2021-11-17 10:24:44 -0500

    Creacion de proyecto + Splash

---
+++ app/src/main/java/com/grupounoptresunal/kamaleon/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.grupounoptresunal.kamaleon
+
+import android.animation.Animator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupounoptresunal.kamaleon.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+        events()
+    }
+    private fun init(){
+        binding.splashAnimation.playAnimation()
+    }
+    private fun events(){
+        binding.splashAnimation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                TODO("Not yet implemented")
+            }
+
+        } )
+    }
+}
