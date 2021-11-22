38a9acafcdd4cd6911e5ef32203e0668565bcc15
Gabriel José Contreras Pérez <gabo21jw@gmail.com>
2021-11-21 22:26:05 -0500

    Registro y olvidar contraseña

--- app/src/main/java/com/grupounoptresunal/kamaleon/RegistroFragment.kt
+++ app/src/main/java/com/grupounoptresunal/kamaleon/RegistroFragment.kt
@@ -7,16 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.grupounoptresunal.kamaleon.databinding.FragmentLoginBinding
-
+import com.grupounoptresunal.kamaleon.databinding.FragmentRegistroBinding
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [RegistroFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+class RegistroFragment : Fragment() {
+    private var _binding: FragmentRegistroBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,21 +27,14 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentRegistroBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onStart() {
         super.onStart()
-        binding.buttonRegisLogin.setOnClickListener() {
-            findNavController().navigate(R.id.action_loginFragment_to_registroFragment2)
-
-        }
-        binding.buttonOlvidePassword.setOnClickListener() {
-            findNavController().navigate(R.id.action_loginFragment_to_passwordOlvidadoFragment2)
+        binding.buttonCancelarRegistro.setOnClickListener() {
+            findNavController().navigate(R.id.action_registroFragment_to_loginFragment2)
 
         }
     }
-
-
 }
